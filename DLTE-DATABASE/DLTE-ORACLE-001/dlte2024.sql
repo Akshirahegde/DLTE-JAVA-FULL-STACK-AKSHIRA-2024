a627bb4034a591205c918847e4846a7394e59f4e
Akshirahegde <akshirahegde@gmail.com>
2024-02-21 19:49:42 +0530

    Views created

--- DLTE-DATABASE/DLTE-ORACLE-001/dlte2024.sql
+++
@@ -1,2 +0,0 @@
---insert into transaction_second(transaction_id,transaction_amount,transaction_date,transaction_to,transaction_remarks)values(transaction_seq.nextval,100,'20Feb2024','Anu','Friend');
-rollback;
---
+++ DLTE-JAVA/DLTE-JAVA-004/DLTE-JAVA-004/src/main/java/explore/oop/incorporate/LoanOffer.java
@@ -0,0 +1,4 @@
+package explore.oop.incorporate;
+
+public class LoanOffer {
+}
