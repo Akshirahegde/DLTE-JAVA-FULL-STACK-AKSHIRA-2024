582b1c275ef95ca587ddbfc708ab0ab0f06d842e
Akshirahegde <akshirahegde@gmail.com>
2024-03-07 16:04:07 +0530

    Updated

---
+++ DLTE-TASK/Task-592/src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args ) throws IOException, ClassNotFoundException {
+        MyBank myBank=new LoanApp();
+        while(true){
+            Scanner scanner=new Scanner(System.in);
+            int choice;
+            System.out.println("1.Add loans\n2.Check available loans\n3.Check closed Loans\n4.Display loans");
+            System.out.println("enter your choice");
+            choice=scanner.nextInt();
+            switch (choice){
+                case 1:
+                    myBank.addLoan();
+                    break;
+                case 2:
+                    myBank.availableLoans();
+                    break;
+                case 3:
+                    myBank.closedLoans();
+                    break;
+
+
+            }
+        }
+
+    }
+}
---
+++ DLTE-TASK/Task-592/src/main/java/org/example/MyBank.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface MyBank {
+    ArrayList<Loan>loans=new ArrayList<>();
+void addLoan() throws IOException, ClassNotFoundException;
+void availableLoans() throws IOException, ClassNotFoundException;
+void closedLoans() throws IOException, ClassNotFoundException;
+
+
+}
